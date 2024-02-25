f00b4a0a0416ccf192ae534266a5746529f411d9
Jableed43 <javiernehuenlopez@gmail.com>
2024-02-25 19:32:18 -0300

    proyecto empezado, sin instalacion de db

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import "reflect-metadata"
+
+const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+
+app.listen(3000)
+
+console.log('server is listening on port', 3000)
+
